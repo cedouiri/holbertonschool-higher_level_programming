c6ad8c8e7c6180c627b6db5cf3efeffe4bca359c
cedouiri <cedouiri@gmail.com>
2019-12-17 22:45:03 +0000

    remove white space

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     _len = len(my_list)
-    
+
     if _len == 0:
         return None
     else:
         maxn = my_list[0]
-    for i in range (1, _len):
+    for i in range(1, _len):
         if my_list[i] > maxn:
             maxn = my_list[i]
         return maxn
