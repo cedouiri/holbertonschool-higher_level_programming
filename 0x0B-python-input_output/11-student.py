3edcc0af5c549c850248ebe34d4afcc54e5d618e
cedouiri <cedouiri@gmail.com>
2020-01-22 22:39:21 +0000

    add 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a class Student that defines a student
+"""
+
+
+class Student:
+    """
+    a class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation with first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Public method that retrieves a dictionary representation
+        """
+        return self.__dict__
