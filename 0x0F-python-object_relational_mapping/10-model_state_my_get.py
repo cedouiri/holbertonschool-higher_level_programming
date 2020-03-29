ec943e0d8460db703abe832f1879bee714481da2
cedouiri <cedouiri@gmail.com>
2020-03-29 23:02:19 +0000

    model state my get

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+a script that lists all State objects
+from the database hbtn_0e_6_usa
+'''
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                        argv[2],
+                                                        argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    search_state = argv[4]
+    found = list()
+    for state in session.query(State).filter(
+            State.name.contains('%a%')).order_by(
+            State.id).all():
+        if state:
+            found.append(state.id)
+            print("{}".format(found[0]))
+    if not found:
+        print("Not found")
+    session.close()
