5eb197616774f0500fa0ae08986717b794afe282
cedouiri <cedouiri@gmail.com>
2020-03-29 21:00:13 +0000

    my filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+from sys import argv
+
+'''
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+'''
+if __name__ == "__main__":
+    con = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1],
+        password=argv[2], database=argv[3])
+    cursor = con.cursor()
+    cursor.execute(
+            "SELECT * FROM states ORDER BY id ASC")
+    db = cursor.fetchall()
+    for i in db:
+        if i[1] == argv[4]:
+            print(i)
