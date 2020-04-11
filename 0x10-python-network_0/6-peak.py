62f1ab56a30b276116566b7d821fa5ba9caf840d
cedouiri <cedouiri@gmail.com>
2020-04-11 19:59:42 +0000

    peak algorithme

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+a function that finds a peak in a list of unsorted integers
+'''
+
+
+def find_peak(loi):
+    '''
+    Define find_peak function
+    '''
+    return find_peak(loi)
