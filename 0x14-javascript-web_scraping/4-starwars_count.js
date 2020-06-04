66c376740d06409e157b09fcad29abf1d0fa4898
cedouiri <cedouiri@gmail.com>
2020-06-04 20:49:08 +0000

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 // a script that prints the number of movies where the character 0€œWedge Antilles0€ is present
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
-const episode = process.argv[2];
-request(url + episode, (err, res, body) => {
+const url = process.argv[2];
+request(url, (err, res, body) => {
   if (err) { console.log(err); } else if (res.statusCode === 200) {
-    const json = JSON.parse(body);
-    console.log(json.title);
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const idx in films) {
+      const filmChars = films[idx].characters;
+      for (const cidx in filmChars) {
+        if (filmChars[cidx].includes('18')) { count++; }
+      }
+    }
+    console.log(count);
   } else { console.log('An error occured. Status code: ' + res.statusCode); }
 });
