b83dd587bf2dfd1f196515b5a865cb09b6532c5b
cedouiri <cedouiri@gmail.com>
2020-07-03 23:57:30 +0000

    Change the text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+//  a Javascript script that updates the text of the HTML tag HEADER to “New Header!!!” when the user clicks on DIV#update_header
+$(() => {
+  $('#update_header').click(() => {
+    $('header').text('New Header!!!');
+  });
+});
