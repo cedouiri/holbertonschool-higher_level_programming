ed81b55a12516a41aa05cd6df38ece56890d44c2
cedouiri <cedouiri@gmail.com>
2020-07-04 00:20:31 +0000

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// a Javascript script that updates the text color of the HTML tag HEADER to red (#FF0000)
+(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('header').style.color = '#FF0000';
+  }, false);
+})();
