b2bb0e9ffde4adfe1c378844b9af9c983d8b5849
cedouiri <cedouiri@gmail.com>
2020-07-04 00:58:12 +0000

    Say hello to everybody!

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,8 +1,8 @@
 // a Javascript script that fetches and prints how to say Hello depending of the language
 $(document).ready(function () {
   $('INPUT#btn_translate').click(function () {
-    const lan_code = $('INPUT#language_code').val();
-    const url = 'https://www.fourtonfish.com/hellosalut/?lang=' + lan_code;
+    const code = $('INPUT#language_code').val();
+    const url = 'https://www.fourtonfish.com/hellosalut/?lang=' + code;
     $.getJSON(url, function (data) {
       $('DIV#hello').html('<br />' + data.hello);
     });
