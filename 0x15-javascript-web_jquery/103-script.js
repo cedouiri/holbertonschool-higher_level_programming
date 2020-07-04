a44ddf221fa1506e612bf5825aa92a67222311dd
cedouiri <cedouiri@gmail.com>
2020-07-04 01:05:59 +0000

    And press ENTER

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,21 @@
+// a Javascript script that fetches and prints how to say Hello depending of the language
+$(document).ready(function () {
+  $('INPUT#language_code').keypress(function (event) {
+    const keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode === '13') {
+      const code = $('INPUT#language_code').val();
+      const url = 'https://www.fourtonfish.com/hellosalut/?lang=' + code;
+      $.getJSON(url, function (data) {
+        $('DIV#hello').html('<br />' + data.hello);
+      });
+    }
+  });
+
+  $('INPUT#btn_translate').click(function () {
+    const code = $('INPUT#language_code').val();
+    const url = 'https://www.fourtonfish.com/hellosalut/?lang=' + code;
+    $.getJSON(url, function (data) {
+      $('DIV#hello').html('<br />' + data.hello);
+    });
+  });
+});
